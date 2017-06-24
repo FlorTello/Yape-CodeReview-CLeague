a4d96680535c3aa5295fed57b074508d5e891435
Yelitza Cho <yelitza.choque@gmail.com>
2017-06-23 19:03:50 -0500

    Regex validaciones

---
+++ public/js/components/validations.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const regexPasword = /^(\b[0-9]{6}\b)*$/;
+const regexMail = /^[a-zA-Z0-9_\-\.~]{2,}@[a-z_\-\.~]{2,}\.[a-z]{2,4}$/;
+
+$('#button').attr('disabled','disabled');
+
+$('#inputPassword').keyup(function(e){
+	if (regexPasword.test($(e.target).val())) {
+	}
+});
+
+$('#inputMail').  
+if (regexMail.test($('#inputMail').val())) {
+	$('#button').removeAttr('disabled');
+} else {
+	$('#button').attr('disabled', 'disabled');
+}
